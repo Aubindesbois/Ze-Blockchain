21eb01fef70212720ccae2b72639e1c5d574bc15
mannae59 <38704535+mannae59@users.noreply.github.com>
2018-04-24 20:50:25 +0000

    Create blockchain.c
    pour l'instant il n'y a que addBlock

---
+++ blockchain.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fonctions.h"
+
+void addBlock (Blockchain blockchain, int maxTrans){
+  // appel de genAleaBlock (int difficulty, int maxTrans, int index, char* previousHash);
+  Block newBlock = genAleaBlock( blockchain->difficulty, maxTrans, (blockchain->genesis->index)+1, blockchain->genesis->previous->hashBlock);
+  
+  //met a jour la blockchain avec le nouveau block 
+  
+  newBlock -> previous = blockchain->genesis->previous;
+  newBlock -> next = blockchain->genesis ;
+  blockchain->genesis->previous->next = newBlock;
+  blockchain->genesis->previous = newBlock;
+  
+  (blockchain->size)++;
+  
+
+}
